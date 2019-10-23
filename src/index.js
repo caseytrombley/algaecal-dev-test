6f7d0801cf96f25e24cb49627ef5415849cb612b
Casey Trombley <casey.trombley@libertymutual.com>
2019-10-23 00:27:34 -0400

    feat: added a function to hide an element when its specified data attribute is equal to a string of zero

--- src/index.js
+++ src/index.js
@@ -6,4 +6,15 @@ import 'bootstrap';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
+
+  const hideElementWhenZero = (elementClassname, elementDataAttribute) => {
+    const elements = document.getElementsByClassName(elementClassname);
+    let i;
+    for (i = 0; i < elements.length; i += 1) {
+      if (elements[i].getAttribute(elementDataAttribute) === '0') {
+        elements[i].style.visibility = 'hidden';
+      }
+    }
+  };
+  hideElementWhenZero('percent-off', 'data-total-savings-amount');
 });
