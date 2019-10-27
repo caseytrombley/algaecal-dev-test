b9a389ae099dd79a976d73b08d3e9078220157a2
Casey Trombley <casey.trombley@libertymutual.com>
2019-10-27 10:54:45 -0400

    feat: added function to show or hide the call center

--- src/index.js
+++ src/index.js
@@ -7,6 +7,60 @@ import 'bootstrap';
 document.addEventListener('DOMContentLoaded', () => {
   loadSVGs();
 
+  const apiUrl = 'https://www.algaecal.com/wp-json/acf/v3/options/options';
+  const callCenterDiv = document.getElementsByClassName('call-center')[0];
+  callCenterDiv.classList.add('invisible');
+
+  const fetchModalContent = (elementId, apiObj) => {
+    const copy = document.getElementById(elementId);
+    fetch(apiUrl)
+      .then(resp => resp.json())
+      .then((data) => {
+        copy.innerHTML = data.acf[apiObj];
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  fetchModalContent('guaranteeCopy', '7yr_full_copy');
+
+  const callCenterResolver = () => {
+    fetch(apiUrl)
+      .then(resp => resp.json())
+      .then((data) => {
+        const officeHours = data.acf.office_hours;
+        const dateDayNumberString = new Date().getDay().toString();
+
+        const timeNow = () => {
+          const d = new Date();
+          const h = (d.getHours() < 10 ? '0' : '') + d.getHours();
+          const m = (d.getMinutes() < 10 ? '0' : '') + d.getMinutes();
+          const s = (d.getSeconds() < 10 ? '0' : '') + d.getSeconds();
+          return `${h}:${m}:${s}`;
+        };
+        const formatTime = (fourDigitTime) => {
+          const hours24 = fourDigitTime.substring(0, 2);
+          const minutes = fourDigitTime.substring(2);
+          return `${hours24}:${minutes}:00`;
+        };
+
+        officeHours.forEach((obj) => {
+          if (obj.day === dateDayNumberString) {
+            const start = formatTime(obj.starting_time);
+            const close = formatTime(obj.closing_time);
+            console.log('starting time:', start);
+            console.log('closing time:', close);
+            console.log('current time:', timeNow());
+
+            const timeRange = [start, close];
+            const isInRange = timeNow() >= timeRange[0] && timeNow() < timeRange[1];
+            if (!isInRange) callCenterDiv.classList.remove('invisible');
+          }
+        });
+      });
+  };
+  callCenterResolver();
+
   const hideElementWhenZero = (elementClassname, elementDataAttribute) => {
     const elements = document.getElementsByClassName(elementClassname);
     let i;
@@ -17,18 +71,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
   hideElementWhenZero('percent-off', 'data-total-savings-amount');
-
-  const fetchModalContent = (elementId, apiObj) => {
-    const copy = document.getElementById(elementId);
-    const url = 'https://www.algaecal.com/wp-json/acf/v3/options/options';
-    fetch(url)
-      .then(resp => resp.json())
-      .then((data) => {
-        copy.innerHTML = data.acf[apiObj];
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  fetchModalContent('guaranteeCopy', '7yr_full_copy');
 });
